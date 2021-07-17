5eb3ddbf84ab628f2dce34c3e8cec572031bfa51
Sebastian Landwehr <info@sebastianlandwehr.com>
2021-07-17 02:42:27 +0200

    fix: do not clone archived repos

--- src/clone.js
+++ src/clone.js
@@ -2,6 +2,6 @@ import execa from 'execa'
 
 export default () =>
   execa.command(
-    'gh-repo-clone-all repos --limit 9999 --source --branch renovate/lock-file-maintenance',
+    'gh-repo-clone-all repos --limit 9999 --source --no-archived --branch renovate/lock-file-maintenance',
     { stdio: 'inherit' }
   )
