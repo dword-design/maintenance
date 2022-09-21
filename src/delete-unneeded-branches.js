a9b848685934ac7a33b312a9ee96403e70661f00
Sebastian Landwehr <info@sebastianlandwehr.com>
2022-09-21 23:19:17 +0000

    fix: foo

--- src/delete-unneeded-branches.js
+++ src/delete-unneeded-branches.js
@@ -3,26 +3,21 @@ import { flatten, mapValues, values } from '@dword-design/functions'
 import getUnneededBranches from './get-unneeded-branches'
 
 export default async octokit => {
-  console.log(
+  const branches =
     getUnneededBranches(octokit)
-      |> await
-      |> mapValues((repo, branches) =>
-        branches.map(branch => ({ branch, repo }))
-      )
-      |> values
-      |> flatten
-  )
-
-  /* const branches = getUnneededBranches(octokit)
     |> await
-    |> mapValues((repo, branches) => branches.map(branch => ({ repo, branch })))
+    |> mapValues((repoBranches, repo) =>
+      repoBranches.map(branch => ({ branch, repo }))
+    )
     |> values
     |> flatten
-  await Promise.all(branches.map(branch => 
-    octokit.rest.git.deleteRef({
-      owner: 'dword-design',
-      ref: `heads/${branch.branch}`,
-      repo: branch.repo,
-    })
-  )) */
+  await Promise.all(
+    branches.map(branch =>
+      octokit.rest.git.deleteRef({
+        owner: 'dword-design',
+        ref: `heads/${branch.branch}`,
+        repo: branch.repo,
+      })
+    )
+  )
 }
